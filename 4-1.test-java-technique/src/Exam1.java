58a8218dcb83e2ec1bd490d6f2de900c2a9b2120
ogata <hisanori.ogata@rakus-partners.co.jp>
2026-01-23 09:54:13 +0900

    Java応用1

---
+++ 4-1.test-java-technique/src/Exam1.java
@@ -0,0 +1,12 @@
+public class Exam1 {
+    public static void main(String[] args) {
+        String name="おがたひさのり";
+
+        int length = name.length();
+       
+        System.out.println(name + "の文字数は" + length + "文字です");
+    
+    }
+}
+
+    
---
+++ 4-1.test-java-technique/src/Exam5.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Exam5 {
+    public static void main(String[] args) {
+        LocalDate a=LocalDate
+
+
+
+        
+
+        
+    }
+
+}
